6bfb5848d883e60e8f00bdd3213f2f726532d2ef
StephenHB <Stephen.zhang22@gmail.com>
2024-12-09 14:03:07 -0500

    Add adapt layer

--- tte_v0.py
+++ tte_v0.py
@@ -96,3 +96,54 @@ def df_to_dataset (dataframe, shuffle=True, batch_size=32):
     # Preparing the next batch of data
     ds = ds.prefetch(batch_size)
     return ds
+
+class AdaptLayers():
+    """
+    Class to adapt the layers for the embedding model.
+    """
+    def __init__(self,config, training_features):
+        """
+        Model for adapting normalizer and string vectorizer layers.
+        Args:
+            config: config dictionary
+            training_features: tf._MapDataset of training features.
+        """
+        self.config = config
+        self.training_features = training_features
+        self.normalizer_features = []
+        self.str_features = []
+        for tower_type in  ["account", "product"]:
+            self.normalizer_features += self.config['normalizer'][tower_type]
+            self.str_features += self.config['str_vectorizer'][tower_type]
+        self._all_features = self.normalizer_features + self.str_features
+        self._adapted_layers = {}
+
+    def adapt(self) -> Dict[str, tf.keras.layers.Layer]:
+        """
+        Adapts the layers for the embedding model.
+        """
+        if len(self.normalizer_features) >0:
+            for feature in self.normalizer_features:
+                normalizer = tf.keras.layers.Normalization(
+                    axis = None,
+                    name = feature + "_normalizer"
+                )
+                # Learn the stats of the data
+                normalizer.adapt(np.concatenate(
+                    list(self.training_features.map(
+                        lambda x: x[feature]
+                    ).batch(100))))
+        if len(self.str_features)>0:
+            for feature in self.str_features:
+                vectorizer = tf.keras.layers.TextVectorization(
+                    max_tokens = 10000,
+                    standardize = 'lower_and_strip_punctuation',
+                    split = 'whitespace',
+                    ngrams =  None,
+                    name = feature + "_textvectorizer"
+                )
+                vectorizer.adapt(np.concatenate(list(self.training_features.map(lambda x: x[feature]).batch(100))))
+        
+        return self._adapted_layers
+    
+adapted_layers =  AdaptLayers(config, ratings).adapt()
