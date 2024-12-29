57c63867718097fe95f5bffe3343d8a88a182bfa
StephenHB <Stephen.zhang22@gmail.com>
2024-12-28 21:47:46 -0500

    create configs for the tte

---
+++ tte/config.py
@@ -0,0 +1,39 @@
+class Config:
+    def __init__(self):
+        
+        self.embedding_dimension = 16
+        self.max_history_length = 100
+        self.product_id_col = 'movie_id'
+        self.batch_size = 512
+        self.validation_freq = 5
+        self.num_epochs = 300
+
+        self.str_vectorizer_config = self.get_str_vectorizer_config()
+        self.list_vectorizer_config = self.get_list_vectorizer_config()
+        self.str_lookup_config = self.get_str_lookup_config()
+        self.normalizer_config = self.get_normalizer_config()
+        
+        def get_str_vectorizer_config(self):
+            return {
+                'account': [],
+                'product': ['movie_title']
+            }
+
+        def get_list_vectorizer_config(self):
+            return {
+                'account': [],
+                'product': []
+            }
+
+        def get_str_lookup_config(self):
+            return {
+                'account': [],
+                'product': []
+            }
+
+        def get_normalizer_config(self):
+            return {
+                'account': ['user_rating'],
+                'product': []
+            }
+       
